850438817199f32d485792326ee969cc9bb6bcd5
Syafi Hakim <msyafihakim@gmail.com>
2023-01-16 00:52:28 +0800

    Update menu and withdraw page
    You can now use arrow keys to navigate for the main menu and withdraw.

--- main.cpp
+++ main.cpp
@@ -19,49 +19,83 @@ int screenWidth = 30;
 int bannerWidth = 25;
 int tries = 0;
 bool needLogin = true;
+bool atMainMenu = false;
+int menuChoice = 0;
+int moneyChoice = 0;
 
+vector<string> menu = { "Display Balance\n",
+                        "Withdraw Money\n",
+                        "Deposit Money\n",
+                        "Transfer Money\n",
+                        "Logout\n"};
+
+vector<vector<string>> money = {{"RM 100", "RM 200", "RM 300"},
+                                {"RM 400", "RM 500", "Other"}};
+
+vector<string> adv_tokenizer(string, char);
+void updateRecipientBalance(int, string);
+vector<vector<string>> readFile(string);
+vector<string> readCredentials();
+boolean checkPassword(string, string);
+void printSuccessfulLogin(string);
+bool transferConfirmation(string);
+void loadingScreen(string);
+void addToHistory(string);
 void printCenter(string);
+void getBalance(string);
+void printLeft(string);
+void updateBalance(int);
+void createDefaultFiles();
+void failLoginScreen();
+void printFailLogin();
+void displayBalance();
+void displayBalance();
+void transferMoney();
+void printMainMenu();
+void withDrawMoney();
+void depositMoney();
+void printNewLine();
 void printBanner();
 void loginScreen();
-void printLeft(string);
-void printSuccessfulLogin(string);
-void printNewLine();
-void printFailLogin();
-void failLoginScreen();
-void printMainMenu();
-void displayBalance();
-void withDrawMoney();
-void loadingScreen(string);
-boolean checkPassword(string, string);
-vector<string> adv_tokenizer(string, char);
-void getBalance(string);
-void updateBalance(int);
-vector<vector<string>> readFile(string);
-void depositMoney();
-bool transferConfirmation(string);
-void transferMoney();
-void updateRecipientBalance(int, string);
-void logout();
-void addToHistory(string);
+void printWarning(string text);
+void mainFunc();
+void notification();
 void receipt();
-void createDefaultFiles();
+void logout();
+void randomColor();
+int getArrowKeys();
+void printWithdrawAmount();
 
 int main() {
-
-    // ATM MACHINEEEE
-
-    // MALAM NI NAK TAMBAH
-    // FEATURE APA???????
+    randomColor();
     createDefaultFiles();
-
-    while(true) {
-        currentUser.history.clear();
-        loginScreen();
-    }
-
+    mainFunc();
 
     return 0;
 }
+
+void randomColor() {
+    srand(time(0));
+    // string colors[4] = {"Color E0", "Color A0", };
+    // string x = colors[rand()%4];
+    system("Color 60");
+}
+
+void mainFunc() {
+    currentUser.history.clear();
+    loginScreen();
+}
+
+int getArrowKeys() {
+    int c = getch();
+    if(c == 224) {
+        return getch();
+    } else {
+        return c;
+    }
+
+}
+
 
 void addToHistory(string activity) {
     currentUser.history.push_back(activity);
@@ -83,13 +117,10 @@ void receipt() {
 
 void loginScreen() {
     while (tries < 3 && needLogin) {
-        system("cls");
-        printBanner();
         string uname, pword;
-        printLeft("USERNAME:");
-        cin >> uname;
-        printLeft("PASSWORD:");
-        cin >> pword;
+        vector<string> credentials = readCredentials();
+        uname = credentials[0];
+        pword = credentials[1];
 
         if(checkPassword(uname, pword)) {
             getBalance(currentUser.id);
@@ -97,46 +128,98 @@ void loginScreen() {
             printSuccessfulLogin(uname);
             sleep(2);
             printMainMenu();
-            needLogin = true;
         } else {
             failLoginScreen();
         }
     }
 }
 
+void waitForMoney() {
+    for(int i=20; i>-20; i--) {
+        COORD c;
+        c.X = i >= 3 ? i : 3;
+        c.Y = 11;
+
+        SetConsoleCursorPosition(
+            GetStdHandle(STD_OUTPUT_HANDLE), c);
+
+        string txt = "Notification : RM100";
+
+        if(txt.size() + i >= screenWidth) {
+            cout << txt.substr(0, screenWidth-i);
+        } else if(i < 0) {
+            cout << txt.substr(abs(i), txt.size()-abs(i));
+        } else {
+            cout << txt;
+        }
+        SetConsoleCursorPosition(
+            GetStdHandle(STD_OUTPUT_HANDLE), c);
+        usleep(100000*2);
+        cout << string(screenWidth, ' ');
+    }
+}
+
+vector<string> readCredentials() {
+    string uname, pword;
+    system("cls");
+    printBanner();
+    printLeft("USERNAME:");
+    cin >> uname;
+    printLeft("PASSWORD:");
+    char c = getch();
+    while(c != 13) {
+        pword += c;
+        system("cls");
+        printBanner();
+        printLeft("USERNAME:" + uname + "\n");
+        printLeft("PASSWORD:" + string(pword.size(), '*'));
+        printNewLine();
+        c = getch();
+    }
+    vector<string> res;
+    res.push_back(uname);
+    res.push_back(pword);
+    return res;
+}
+
 void printMainMenu() {
-    while(true) {
+    int input = 0;
+    while(input != 13) {
         system("cls");
         printBanner();
         printCenter("Welcome, " + currentUser.name);
         printNewLine();
-        printLeft("1 - Display Balance\n");
-        printLeft("2 - Withdraw Money\n");
-        printLeft("3 - Deposit Money\n");
-        printLeft("4 - Transfer Money\n");
-        printLeft("5 - Logout\n");
-
-        char choice;
-        choice = getch();
-        cout << choice;
-
-        if(choice == 49) {
-            displayBalance();
-            getch();
-        } else if(choice == 50) {
-            withDrawMoney();
-            getch();
-        } else if(choice == 51) {
-            depositMoney();
-            getch();
-        } else if(choice == 52) {
-            transferMoney();
-            getch();
-        } else if(choice == 53) {
-            loadingScreen("LOGGING OUT");
-            receipt();
-            break;
+        for(int i=0; i<menu.size(); i++) {
+            if(menuChoice == i) {
+                printLeft("> " + menu[i]);
+            } else {
+                printLeft("  " + menu[i]);
+            }
         }
+        input = getArrowKeys();
+
+        if(input == 72 && menuChoice > 0) {
+            menuChoice--;
+        } else if(input == 80 && menuChoice < menu.size()) {
+            menuChoice++;
+        } else if(input == 13){
+            if(menuChoice == 0) {
+                displayBalance();
+            } else if(menuChoice == 1) {
+                withDrawMoney();
+            } else if(menuChoice == 2) {
+                depositMoney();
+            } else if(menuChoice == 3) {
+                transferMoney();
+            } else if(menuChoice == 4) {
+                loadingScreen("LOGGING OUT");
+                receipt();
+                needLogin = true;
+                mainFunc();
+            } else {
+                printMainMenu();
+            }
+    }
     }
 }
 
@@ -160,9 +243,9 @@ void transferMoney() {
     }while(confirmed == false);
 
     currentUser.balance -= amount;
-
     updateBalance(amount);
     updateRecipientBalance(amount, id);
+    printMainMenu();
 }
 
 bool transferConfirmation(string id) {
@@ -185,8 +268,8 @@ bool transferConfirmation(string id) {
     printLeft("Name : " +  allUsers[id] + "\n");
     printCenter("===================");
     printNewLine();
-    printCenter("Press 8 - Yes");
-    printCenter("Press 9 - No");
+    printLeft("Press 8 - Yes \n");
+    printLeft("Press 9 - No");
     choice = getch();
 
     if(choice == 56) {
@@ -197,15 +280,50 @@ bool transferConfirmation(string id) {
 
 }
 
+void printWithdrawAmount(int row, int col) {
+    for(int i=0; i<money.size(); i++) {
+        printLeft("");
+        for(int j=0; j<money[i].size(); j++) {
+            if( j != 0) cout << "  ";
+
+            if(row == i && j == col)
+                cout << "[" + money[i][j] + "]";
+            else
+                cout << " " + money[i][j] + " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
 void withDrawMoney() {
-    system("cls");
-    printBanner();
-    printNewLine();
-    printLeft("Enter amount to withdraw:\n");
-    printCenter("===================");
-    printLeft("RM ");
-    int amount;
-    cin >> amount;
+    vector<vector<int>> amounts = {
+    {100,200,300},
+    {400,500}};
+    int amount = 0;
+    int input = 0;
+    int row = 0;
+    int col = 0;
+    while(input != 13) {
+        system("cls");
+        printBanner();
+        printWithdrawAmount(row, col);
+        input = getArrowKeys();
+        if(input == 72) {
+            row--;
+        } else if(input == 80) {
+            row++;
+        } else if(input == 77) {
+            col++;
+        } else if(input == 75) {
+            col--;
+        } else if(input == 13) {
+            break;
+        }
+        amount = amounts[row][col];
+    }
+    printLeft("RM " + to_string(amount));
+
     printCenter("===================");
     currentUser.balance =  currentUser.balance - amount;
     updateBalance(currentUser.balance);
@@ -213,6 +331,16 @@ void withDrawMoney() {
     loadingScreen("Please Wait");
     printLeft("WITHDRAW SUCCESSFUL!!");
     addToHistory("WITHDRAW - RM " + to_string(amount));
+    getch();
+    printMainMenu();
+}
+
+void printWarning(string text) {
+    printCenter("+" + string(25,
+                             '-') + "+");
+    printCenter(text);
+    printCenter("+" + string(25,
+                             '-') + "+");
 }
 
 void depositMoney() {
@@ -231,6 +359,8 @@ void depositMoney() {
     loadingScreen("Please Wait");
     printLeft("DEPOSIT SUCCESSFUL!!");
     addToHistory("DEPOSIT - RM " + to_string(amount));
+    getch();
+    printMainMenu();
 }
 
 void updateRecipientBalance(int deposit, string id) {
@@ -310,6 +440,8 @@ void displayBalance() {
     printCenter("==========");
     printCenter("RM " + to_string(currentUser.balance));
     printCenter("==========");
+    getch();
+    printMainMenu();
 }
 
 void failLoginScreen() {
@@ -442,7 +574,7 @@ void createDefaultFiles() {
         duit << "006;1000;" << endl;
         duit << "007;1000;";
         ofstream users("users.txt");
-        users << "001;admin;admin;" << endl;
+        users << "001;yana;yana;" << endl;
         users << "002;syafi;123;" << endl;
         users << "003;amar;123;" << endl;
         users << "004;aizzad;000;" << endl;
